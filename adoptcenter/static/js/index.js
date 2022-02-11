35712eeeed5f1aacf09eb9e44a1d7fd681bbe055
Amanuel Afework <schwartzfuss763@yahoo.com>
2022-02-11 09:24:18 -0600

    created adoptcenter and superuser and connected to postgresql

---
+++ adoptcenter/static/js/index.js
@@ -0,0 +1,18 @@
+// Slick Sliders
+$('.slick-carousel').each(function () {
+    var slider = $(this);
+    $(this).slick({
+        infinite: true,
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0'
+    });
+
+    $(this).closest('.slick-slider-area').find('.slick-prev').on("click", function () {
+        slider.slick('slickPrev');
+    });
+    $(this).closest('.slick-slider-area').find('.slick-next').on("click", function () {
+        slider.slick('slickNext');
+    });
+});
